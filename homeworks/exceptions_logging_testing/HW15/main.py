b7e5b9743f4b18b20bec32d930ca27b9710c4f2c
Denys Dalebiha <karabis@i.ua>
2022-07-23 22:18:42 +0300

    Add tests

--- homeworks/exceptions_logging_testing/HW15/main.py
+++ homeworks/exceptions_logging_testing/HW15/main.py
@@ -1,6 +1,7 @@
 import uuid
 import datetime as dt
 
+
 class UserNotFound(Exception):
     pass
 
@@ -16,6 +17,9 @@ class User:
         self.incoming_messages = []
         self.outgoing_messages = []
 
+    def __str__(self):
+        return f"{self.name}"
+
     def add_message(self, message_obj, is_incoming=True):
         """
         Додає повідомлення в incoming_messages чи outgoing_messages в залежності від is_incoming
@@ -23,6 +27,7 @@ class User:
         :param is_incoming:
         :return:
         """
+        is_incoming = message_obj.user_to == self
         if is_incoming:
             self.incoming_messages.append(message_obj)
         else:
@@ -75,6 +80,16 @@ class User:
         :param include_outgoing:
         :return:
         """
+        mes = []
+        if include_incoming:
+            mes = list(filter(lambda m: m.id == message_id, self.incoming_messages))
+            if mes:
+                return mes[0]
+        if include_outgoing:
+            mes = list(filter(lambda m: m.id == message_id, self.outgoing_messages))
+            if mes:
+                return mes[0]
+        raise MessageNotFound
 
 
 class Message:
@@ -84,6 +99,9 @@ class Message:
         self.user_to = user_to
         self.date = date
         self.text = text
+
+    def __str__(self):
+        return f"{self.date} {self.user_from} -> {self.user_to}\n{self.text}"
 
     def edit(self, new_text):
         """
@@ -105,25 +123,24 @@ class MessageHelper:
         :param text:
         :return:
         """
-        if user_from_id in UserHelper.users_db and user_to_id in UserHelper.users_db:
-            user_from = UserHelper.get_user(user_from_id)
-            user_to = UserHelper.get_user(user_to_id)
-            message = Message(user_from, user_to, dt.datetime.now(), text)
-            user_from.outgoing_messages.append(message)
-            user_to.incoming_messages.append(message)
-        else:
+        if user_from_id not in UserHelper.users_db or user_to_id not in UserHelper.users_db:
             raise UserNotFound
+        else:
+            u_from = UserHelper.get_user(user_from_id)
+            u_to = UserHelper.get_user(user_to_id)
+            h_message = Message(u_from, u_to, dt.datetime.now(), text)
+            u_from.add_message(h_message, False)
+            u_to.add_message(h_message, True)
 
     @staticmethod
-    def unsend_message(message):
+    def unsend_message(message: Message) -> None:
         """
         Видаляє повідомлення у двох юзерів
         :param message:
         :return:
         """
-        # TODO - create message object
-        # TODO = save message to user user_to
-        pass
+        message.user_to.incoming_messages.remove(message)
+        message.user_from.outgoing_messages.remove(message)
 
 
 class UserHelper:
@@ -141,7 +158,7 @@ class UserHelper:
         :return:
         """
         user = User(name)
-        cls.user_db[user.id] = user
+        cls.users_db[user.id] = user
         return user.id
 
     @classmethod
@@ -163,5 +180,16 @@ class UserHelper:
         :param id:
         :return:
         """
-        del(cls.users_db[id])
-pass
+        del (cls.users_db[id])
+
+
+# user_from_id = UserHelper.create_user("UserFrom")
+# user_to_id = UserHelper.create_user("UserTo")
+# ut = UserHelper.get_user(user_to_id)
+# #message = Message(user_from, user_to, dt.datetime.now(), 'test message')
+# MessageHelper.send_message(user_from_id, user_to_id, "Hello bro")
+# m = ut.get_last_message()
+# m_id = m.id
+# MessageHelper.unsend_message(m)
+# #print(ut.get_message_by_id(m_id))
+
---
+++ homeworks/exceptions_logging_testing/HW15/unit_testes.py
@@ -0,0 +1,105 @@
+from unittest import TestCase
+import datetime as dt
+import main
+
+
+class TestUser(TestCase):
+    def setUp(self) -> None:
+        self.user_from = main.User("UserFrom")
+        self.user_to = main.User("UserTo")
+        self.message = main.Message(self.user_from, self.user_to, dt.datetime.now(), 'test message')
+        self.message1 = main.Message(self.user_to, self.user_from, dt.datetime.now(), 'test message1')
+        self.message2 = main.Message(self.user_from, self.user_to, dt.datetime.now(), 'test message2')
+        for mes in [self.message, self.message1, self.message2]:
+            self.user_to.add_message(mes)
+
+    def tearDown(self) -> None:
+        del (self.user_from, self.user_to, self.message)
+
+    def test_add_in(self):
+        self.user_to.add_message(self.message)
+        self.assertIn(self.message, self.user_to.incoming_messages)
+
+    def test_add_out(self):
+        self.user_to.add_message(self.message1, False)
+        self.assertIn(self.message1, self.user_to.outgoing_messages)
+
+    def test_get_last(self):
+        self.assertEqual(self.user_to.incoming_messages[-1], self.message2)
+
+    def test_get_all(self):
+        self.assertEqual(self.user_to.get_all_messages(True, True), {"incomings": [self.message, self.message2],
+                                                                     "outgoings": [self.message1]})
+        self.assertEqual(self.user_to.get_all_messages(False, True), {"incomings": None,
+                                                                     "outgoings": [self.message1]})
+
+    def test_read_last(self):
+        self.assertEqual(self.user_to.read_last_message(), self.message2)
+        self.assertEqual(self.user_to.incoming_messages[-1], self.message)
+
+    def test_read_all(self):
+        self.assertEqual(self.user_to.read_all_messages(), [self.message, self.message2])
+        self.assertEqual(self.user_to.incoming_messages, [])
+
+    def test_get_by_id(self):
+        self.assertEqual(self.user_to.get_message_by_id(self.message.id), self.message)
+
+class TestMessage(TestCase):
+    def setUp(self) -> None:
+        self.user_from = main.User("UserFrom")
+        self.user_to = main.User("UserTo")
+        self.message = main.Message(self.user_from, self.user_to, dt.datetime.now(), 'test message')
+
+    def tearDown(self) -> None:
+        del (self.user_from, self.user_to, self.message)
+
+    def test_edit(self):
+        self.message.edit("new text")
+        self.assertEqual(self.message.text, "new text")
+
+class TestMessegeHelper(TestCase):
+    def setUp(self) -> None:
+        self.user_from_id = main.UserHelper.create_user("UserFrom")
+        self.user_to_id = main.UserHelper.create_user("UserTo")
+        self.user_to = main.UserHelper.get_user(self.user_to_id)
+        main.MessageHelper.send_message(self.user_from_id, self.user_to_id, "Hello bro")
+        self.messege = self.user_to.get_last_message()
+
+    def tearDown(self) -> None:
+        del (self.user_from_id, self.user_to_id)
+
+    def test_send_message(self):
+        self.assertEqual(main.UserHelper.get_user(self.user_to_id).get_last_message().text,
+                         "Hello bro")
+        self.assertEqual(main.UserHelper.get_user(self.user_from_id).outgoing_messages[-1].text,
+                         "Hello bro")
+        us = main.User("Fail")
+        with self.assertRaises(main.UserNotFound):
+            main.MessageHelper.send_message(self.user_from_id, us.id, "UDP")
+
+    def test_unsend_message(self):
+        m_id = self.messege.id
+        main.MessageHelper.unsend_message(self.messege)
+        with self.assertRaises(main.MessageNotFound):
+            self.user_to.get_message_by_id(m_id)
+
+class TestUserHelper(TestCase):
+    def setUp(self) -> None:
+        self.uh = main.UserHelper
+        self.uder_id = self.uh.create_user("Messagesender")
+
+    def tearDown(self) -> None:
+        del(self.uh)
+
+    def test_create_user(self):
+        self.assertIn(self.uder_id, self.uh.users_db)
+
+    def test_get_user(self):
+        self.assertEqual(self.uh.get_user(self.uder_id).name, "Messagesender")
+
+    def test_delete_user(self):
+        self.uh.delete_user(self.uder_id)
+        with self.assertRaises(main.UserNotFound):
+            self.uh.get_user(self.uder_id)
+
+
